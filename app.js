de67aa1fef148f610c219123cb563395e13bb261
Idriss-rayan <rayanidriss27@gmail.com>
2025-06-16 00:18:28 +0600

    new file

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-console.log(__dirname)
-setInterval(()=>{
-    console.log('hello world!')
-},1000)
+
+
+// Modules
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+//console.log(names)
+
+sayHi(names.john)
+sayHi(names.peter)
+//sayHi(names.john)
