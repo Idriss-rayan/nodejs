f6ed420e5987dc0462416d3826a3a33085783e6a
Idriss-rayan <rayanidriss27@gmail.com>
2025-06-17 18:45:16 +0600

    how to use os in nodejs

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 
+//----------------------------------//
+// // Modules
+// const names = require('./4-names')
+// const sayHi = require('./5-utils')
+// //console.log(names)
+// const data = require('./6-alternative-flavor')
+// require('./7-mind-grenade')
 
-// Modules
-const names = require('./4-names')
-const sayHi = require('./5-utils')
-//console.log(names)
-const data = require('./6-alternative-flavor')
-require('./7-mind-grenade')
+// // sayHi('rayan')
+// // sayHi(names.john)
+// // sayHi(names.peter)
+// //sayHi(names.john)
 
-// sayHi('rayan')
-// sayHi(names.john)
-// sayHi(names.peter)
-//sayHi(names.john)
+//---------------------------------//
+
+const os = require('os')
+
+const user = os.userInfo()
+
+
+// method returns the system uptime in seconds
+console.log(`system uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+console.log(currentOS)
