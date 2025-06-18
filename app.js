c955b8b59a831595690711d2024291bcfd20bca6
Idriss-rayan <rayanidriss27@gmail.com>
2025-06-18 20:28:15 +0600

    read

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const {readFileSync, writeFileSync} = require('fs')
+const {readFile, writeFile} = require('fs')
 
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
-
-console.log(first , second)
+readFile('./content/first.txt','utf8' , (err, result)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log(result)
+})
