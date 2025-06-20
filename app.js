2d109dab6921c7114993a283504cb5b0d7bcdb56
Idriss-rayan <rayanidriss27@gmail.com>
2025-06-21 00:38:33 +0600

    add new file

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-setInterval(() => {
-    console.log('hello my name is rayan')
-}, 2000);
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    console.log('request event')
+    res.end('hello world')
+})
+
+server.listen(5000, ()=> {
+    console.log('Server listening on port : 5000....')
+})
