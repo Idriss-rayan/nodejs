b29c83fe923c73ad996714bfa3e1f3406e952106
Idriss-rayan <rayanidriss27@gmail.com>
2025-06-21 09:34:55 +0600

    add new file

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const http = require('http')
+const http = require('http');
 
-const server = http.createServer((req, res)=>{
-    console.log('request event')
-    res.end('hello world')
+
+const server = http.createServer((req , res) => {
+if (req.url === '/'){
+    res.end('Home Page')
+}
+if(req.url === '/about') {
+    // BLOCKING CODE !!!
+    for (let i=0; i<100; i++){
+        for (let j=0; j<100; j++){
+            console.log(`${i} ${j}`)
+        }
+    }
+    res.end('About Page')
+}
+
 })
 
-server.listen(5000, ()=> {
-    console.log('Server listening on port : 5000....')
+server.listen(5000, ()=>{
+    console.log('Server listening on part 5000 ...')
 })
