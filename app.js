50b767879c6c70cd785f1c68ae32d438e3535c7e
Idriss-rayan <rayanidriss27@gmail.com>
2025-06-24 17:11:33 +0600

    event emitter

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const rayan = "path"
+const EventEmitter = require('events');
+
+const customEmiter = new EventEmitter()
+
+customEmiter.on('response',()=>{
+    console.log(`data recieved`)
+})
+
+customEmiter.emit('response')
