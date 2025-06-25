54f386b6efdb25f1bdb5864a1ae71d14c28bc4de
Idriss-rayan <rayanidriss27@gmail.com>
2025-06-25 07:41:39 +0600

    event emitter

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ const customEmiter = new EventEmitter()
 customEmiter.on('response',()=>{
     console.log(`data recieved`)
 })
+customEmiter.on('response',()=>{
+    console.log(`some other logic here`)
+})
+
 
 customEmiter.emit('response')
