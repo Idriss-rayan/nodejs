5b891e63e84992fbcdcbfa14bccf966bc18afc43
Idriss-rayan <rayanidriss27@gmail.com>
2025-06-26 23:53:04 +0600

    big file

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const {createReadStream} = require('fs');
 const { result } = require('lodash');
 
-const stream = createReadStream('./content/big.txt')
+const stream = createReadStream('./content/big.txt', {highWaterMark: 90000,encoding:'utf8'})
 
 stream.on('data' , (result) => {
     console.log(result)
 })
+stream.on('error',(err)=>console.log(err))
