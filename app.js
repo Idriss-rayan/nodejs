d7c87a9eed1d112a70cb5165da86772bcc8d6e8f
Idriss-rayan <rayanidriss27@gmail.com>
2025-06-29 22:34:52 +0600

    add new file the 6/29/2025

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const http = require('http')
 
-const server = http.createServer((req, res)=>{})
+const server = http.createServer((req, res)=>{
+    res.writeHead(400,{'content-type':'text/html'})
+    res.write('<h1>home page</h1>')
+    console.log('je suis rayan')
+    res.end()
+})
+
+server.listen(5000)
