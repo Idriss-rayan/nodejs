2093147b5b3be92744ff779b6533811b68835859
Idriss-rayan <rayanidriss27@gmail.com>
2025-07-04 22:50:44 +0600

    add new file

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 let { people } = require('./data')
 
+app.get('/api/people',(req,res) =>{
+  res.status(200).json({success: true, data:people})
+})
 
 
 app.listen(5000,()=>{
