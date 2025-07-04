574d52a415cf72d0f967588a98a17ff25164fa99
Idriss-rayan <rayanidriss27@gmail.com>
2025-07-04 22:15:22 +0600

    new file add

--- authorize.js
+++ authorize.js
@@ -1,6 +1,14 @@
-const authorize = (req , res , next) =>{
-    console.log('autorize')
+const authorize = (req, res, next) => {
+  const { user } = req.query
+
+  if (user === 'john') {
+    req.user = { name: 'john', id: 3 }
+    console.log('authorize: access granted')
     next()
+  } else {
+    console.log('authorize: access denied')
+    res.status(401).send('Unauthorized')
+  }
 }
 
 module.exports = authorize
