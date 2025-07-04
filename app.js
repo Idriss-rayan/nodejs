5d640a76e7a03b878d0a07a1058fae55c43317aa
Idriss-rayan <rayanidriss27@gmail.com>
2025-07-04 23:35:20 +0600

    add new files

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ let { people } = require('./data')
 app.use(express.static('./methods-public'))
 // parse form data
 app.use(express.urlencoded({extended: false}))
+// parse json 
+app.use(express.json())
 app.get('/api/people',(req,res) =>{
   res.status(200).json({success: true, data:people})
+})
+
+app.post('/api/people',(req,res) =>{
+  res.status(200).send('Success')
 })
 
 app.post('/login',(req, res)=>{
